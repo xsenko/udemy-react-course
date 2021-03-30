b23d77545788452e5f861c33a5578adb085ded11
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-03-30 23:05:29 +0200

    add Dropdown

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,7 +1,8 @@
 import 'semantic-ui-css/semantic.min.css'
-import React from 'react';
+import React, { useState } from 'react';
 import Accordion from "./components/Accordion";
 import Search from "./components/Search";
+import Dropdown from "./components/Dropdown";
 
 
 const items = [
@@ -19,11 +20,34 @@ const items = [
     }
 ];
 
+const options = [
+    {
+        label: "The Color Red",
+        value: "red"
+    },
+    {
+        label: "The Color Green",
+        value: "green"
+    },
+    {
+        label: "A Shade of Blue",
+        value: "blue"
+    }
+];
+
 export default () => {
+    const [selected, setSelected] = useState(options[0])
+    const [showDropdown, setShowDropdown] = useState(true)
+
     return (
         <div>
+            <button onClick={() => setShowDropdown(!showDropdown)} >Toggle Dropdwon</button>
             <br/>
-            <Search />
+            {showDropdown ?
+                <Dropdown selected={selected} options={options} onSelectedChange={setSelected} />
+                : null
+            }
+
         </div>
     )
 }
--- widgets/src/components/Search.js
+++ widgets/src/components/Search.js
@@ -1,8 +1,57 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from "axios";
 
 const Search = () => {
 
-    const [term, setTerm] = useState("");
+    const [term, setTerm] = useState("programming");
+    const [debouncedTerm, setDebouncedTerm] = useState(term);
+    const [results, setResults] = useState([]);
+
+    useEffect(() => {
+        const timerId = setTimeout(() => {
+            setDebouncedTerm(term);
+        }, 1000);
+
+        return () => {
+            clearTimeout(timerId);
+        }
+    }, [term]);
+
+    useEffect(() => {
+        const search = async () => {
+            const {data}  = await axios.get('https://en.wikipedia.org/w/api.php',
+                {
+                    params: {
+                        action: 'query',
+                        list: 'search',
+                        origin: '*',
+                        format: 'json',
+                        srsearch: debouncedTerm
+                    }
+                });
+            setResults(data.query.search);
+        };
+        if(debouncedTerm) {
+            search();
+        }
+
+    }, [debouncedTerm]);
+
+    const renderedResults = results.map((result) => {
+        return(
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a className="ui button" href={`https://en.wikipedia.org?curid=${result.pageid}`}>Go</a>
+                </div>
+                <div className="content">
+                    <div className="header">
+                        {result.title}
+                    </div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+            )
+    })
 
     return (
         <div>
@@ -11,6 +60,9 @@ const Search = () => {
                     <label>Enter Search Term</label>
                     <input className="input"  value={term} onChange={e => setTerm(e.target.value)} />
                 </div>
+            </div>
+            <div className="ui celled list">
+                {renderedResults}
             </div>
         </div>
     )
