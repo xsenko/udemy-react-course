0ff51f92fdded03807557b39def5c9677cef04a2
Selcuk Cabuk <cabuk.selcuk@gmail.com>
2021-04-06 15:58:10 +0200

    add initial files

---
+++ blog/src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App = () => {
+    return (
+        <div className="ui container">
+            APP
+        </div>
+    )
+}
+
+export default App;
---
+++ blog/src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from "redux";
+import 'semantic-ui-css/semantic.min.css'
+
+import App from './components/App';
+import reducers from './reducers';
+
+ReactDOM.render(
+    <Provider store={createStore(reducers)}>
+        <App />
+    </Provider>, document.querySelector('#root')
+);
